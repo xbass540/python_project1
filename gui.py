e42993adc7132303d280b174220e8d7cc02eba7f
kostas <info@solomonsoft.gr>
2024-11-06 14:08:56 +0200

    cli basic interactions

---
+++ gui.py
@@ -0,0 +1,10 @@
+from modules import functions
+import FreeSimpleGUI as sg
+
+label = sg.Text("Type in a to do")
+input_box = sg.InputText(tooltip="Enter a todo")
+add_button = sg.Button("Add")
+
+window = sg.Window('My To-Do app', layout=[[label],[input_box,add_button]])
+window.read()#displays window on the screen
+window.close()
