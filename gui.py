b8aae5596114ad54a95cdc4fb5925f7ccca45eaf
kostas <info@solomonsoft.gr>
2024-11-14 16:19:40 +0200

    new commit 44

--- gui.py
+++ gui.py
@@ -6,5 +6,8 @@ input_box = sg.InputText(tooltip="Enter a todo")
 add_button = sg.Button("Add")
 
 window = sg.Window('My To-Do app', layout=[[label],[input_box,add_button]])
+
+
 window.read()#displays window on the screen
+print("Hello")
 window.close()
